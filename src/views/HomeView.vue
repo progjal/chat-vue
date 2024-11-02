b52984593ffa933cbbac47c8edf794c60f1a562a
grt <saifulgrtanwar@gmail.com>
2024-11-02 15:31:57 +0700

    Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import { onMounted } from 'vue';
+import { socket } from "@/socket";
+
+function onMessage(msg: string) {
+    console.log(`Message : "${msg}"`)
+}
+
+onMounted(() => {
+    socket.on('message', onMessage)
+})
+
+</script>
+
+<template>
+  <main>
+    <div>HOME</div>
+  </main>
+</template>
+../lib/socket.js
